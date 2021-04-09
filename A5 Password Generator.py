9911674259bb66d92f8c9ab6f697bbb037a257c1
Vivek <65784413+vkdhiman93@users.noreply.github.com>
2021-04-09 10:10:46 +0530

    Update A5 Password Generator.py

--- A5 Password Generator.py
+++ A5 Password Generator.py
@@ -9,7 +9,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-plst = []
+plst = [] # Defining an Empty Password List
 
 for char in range(nr_letters):
     plst.append(random.choice(letters))
